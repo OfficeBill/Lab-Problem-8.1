84f162d0597bbdf3670a2c6bacad274f35900087
w-inkrott <w-inkrott@JLK307-F8>
2024-10-31 08:06:19 -0400

    Add project files.

---
+++ Lab Problem 8.1/Lab Problem 8.1.cpp
@@ -0,0 +1,17 @@
+// File Name:     Lab Problem 8.1
+// Author:        William Inkrott
+// Date:          09/19/24
+// Description:   Determine the number of vowels in an inputted character. 
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(void)
+{
+	
+}
